8df85d0a5bf37089b40c10e63cb76258b3ea98c7
Joseph William Hauger <joseph>
2019-06-05 21:07:29 -0500

    rollo round 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
+function UserCard (props) {
+  let info = (
+    <div>   
+    <span>{props.user.name.first}</span>
+    (' ')
+    <span>{props.user.name.last}</span>
+    </div>
+  )
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div styel={{marginBottom: '40px'}}>
+        <img src={props.user.picture.large}></img>
+    <div>   
+    {props.hide === false ? info: null}
+    </div>
+    <button onClick={props.onClick}>Hide Details</button>
     </div>
   );
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      results: [],
+      isHidden: true
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://randomuser.me/api?results=25')
+    .then((res) =>{
+      return res.json();
+    })
+    .then((json) =>{
+      this.setState({
+        results: json.results
+      })
+
+    })
+  }
+
+  onClick = () => {
+    this.setState({
+      isHidden: false
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.results.map((user, index) => 
+          <UserCard 
+          key={index} 
+          user={user} 
+          onClick={this.onClick} 
+          hide={this.state.isHidden}
+            />
+     )}
+      </div>
+    );
+}
 }
 
 export default App;
